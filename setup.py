8348948909497dcc56b1b37427cae8b6831712aa
Stefan Falkner <sfalkner@informatik.uni-freiburg.de>
2015-05-29 15:01:40 +0200

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "pynisher",
+    version = "0.1",
+    packages = find_packages(),
+    install_requires = ['docutils>=0.3', 'setuptools'],
+    author = "Stefan Falkner",
+    author_email = "sfalkner@informatik.uni-freiburg.de",
+    description = "A small Python library to limit the resources used by a function by executing it inside a subprocess.",
+    include_package_data = False,
+    keywords = "resources",
+    license = "GPLv3",
+    url = "https://github.com/sfalkner/pynisher"
+)
