b9de071b9cccb08bcb94cc9caa3769be89b06b33
Stefan Falkner <sfalkner@informatik.uni-freiburg.de>
2015-05-29 14:25:31 +0200

    tried to fix the deadlock when no wallclock limit was set and the cpu time limit was exceeded.

--- pynisher/limit_function_call.py
+++ pynisher/limit_function_call.py
@@ -19,31 +19,25 @@ def subprocess_func(func, pipe, mem_in_mb, cpu_time_limit_in_s, wall_time_limit_
     # simple signal handler to catch the signals for time limits
     def handler(signum, frame):
         # logs message with level debug on this logger 
+        logger = multiprocessing.get_logger()
         logger.debug("received signal number %i. Exiting uncracefully."%signum)
         
         if (signum == signal.SIGXCPU):
             # when process reaches soft limit --> a SIGXCPU signal is sent (it normally terminats the process)
-            logger.debug("CPU time exceeded, aborting!")
+            logger.warning("CPU time exceeded, aborting!")
         elif (signum == signal.SIGALRM):
             # SIGALRM is sent to process when the specified time limit to an alarm function elapses (when real or clock time elapses)
-            logger.debug("Wallclock time exceeded, aborting!")
-        pipe.send(None)
-        pipe.close()   
+            logger.warning("Wallclock time exceeded, aborting!")
         raise abort_function
+    
 
-
-	# catch all catchable signals
+    # catch all catchable signals
     for i in [x for x in dir(signal) if x.startswith("SIG")]:
         try:
             signum = getattr(signal,i)
             signal.signal(signum,handler)
         except: # ignore problems setting the handle, surely an uncatchable signum :)
             pass
-    
-    #signal.signal(signal.SIGALRM, handler)
-    # SIGTERM signal is sent to a process to request its termination
-    #signal.signal(signal.SIGTERM, handler)
-    #signal.signal(signal.SIGXCPU, handler)
 
     # set the memory limit
     if mem_in_mb is not None:
@@ -67,8 +61,8 @@ def subprocess_func(func, pipe, mem_in_mb, cpu_time_limit_in_s, wall_time_limit_
         # one could also limit the actual CPU time, but that does not help if the process hangs, e.g., in a dead-lock
         resource.setrlimit(resource.RLIMIT_CPU, (cpu_time_limit_in_s,cpu_time_limit_in_s))
 
-    return_value=None;
 
+    return_value = None
     # the actual function call
     try:
         logger.debug('call to your function')
@@ -82,20 +76,26 @@ def subprocess_func(func, pipe, mem_in_mb, cpu_time_limit_in_s, wall_time_limit_
         if (e.errno == 11):
             logger.warning("Your function tries to spawn too many subprocesses/threads.")
         else:
-            logger.warning('Something is going on here!')
+            logger.debug('Something is going on here!')
             raise;
 
     except abort_function:
-        logger.warning('Your function call was aborted. It probably took too long.')
+        return_value = None
+        logger.warning('Your function call was aborted.')
 
     except:
         logger.debug('The call to your function did not return properly!\n%s\n%s', args, kwargs)
         raise;
     finally:
-        pipe.send(return_value)
-        pipe.close()
+        try:
+            pipe.send(return_value)
+            pipe.close()
+        except:
+            pass
 
-def enforce_limits (mem_in_mb=None, cpu_time_in_s=None, wall_time_in_s=None, num_processes=None, grace_period_in_s = 1):
+
+def enforce_limits (mem_in_mb=None, cpu_time_in_s=None, wall_time_in_s=None, num_processes=None, grace_period_in_s = None):
+
     logger = multiprocessing.get_logger()
     
     if mem_in_mb is not None:
@@ -106,13 +106,14 @@ def enforce_limits (mem_in_mb=None, cpu_time_in_s=None, wall_time_in_s=None, num
         logger.debug("restricting your function to {} seconds wall time.".format(wall_time_in_s))
     if num_processes is not None:
         logger.debug("restricting your function to {} threads/processes.".format(num_processes))
-
+    if grace_period_in_s is None:
+        grace_period_in_s = 0
 
     
     def actual_decorator(func):
 
         def wrapped_function(*args, **kwargs):
-
+            global return_value
             logger = multiprocessing.get_logger()
             
             # create a pipe to retrieve the return value
@@ -121,35 +122,55 @@ def enforce_limits (mem_in_mb=None, cpu_time_in_s=None, wall_time_in_s=None, num
             # create and start the process
             subproc = multiprocessing.Process(target=subprocess_func, name="Call to your function", args = (func, child_conn,mem_in_mb, cpu_time_in_s, wall_time_in_s, num_processes) + args ,kwargs = kwargs)
             logger.debug("Your function is called now.")
-            subproc.start()
-            
-            if wall_time_in_s is not None:
-                # politely wait for it to finish
-                ready = parent_conn.poll(wall_time_in_s + grace_period_in_s)
+
+            return_value = None
+
+            def child_died(signum, frame):
+                logger.debug("Subprocess running your function has died!")
+                if subproc.is_alive():
+                    subproc.join()
+                if parent_conn.poll():
+                    global return_value
+                    return_value = parent_conn.recv()
+                parent_conn.close()
+
+            signal.pthread_sigmask(signal.SIG_UNBLOCK, {signal.SIGCHLD})
+            signal.signal(signal.SIGCHLD, child_died)
+
+            try:            
+                subproc.start()
+                if wall_time_in_s is not None:
+                    # politely wait for it to finish
+                    ready = parent_conn.poll(wall_time_in_s)
                 
-                # if it is still alive, send sigterm
-                if not ready:
-                    logger.debug("Your function took to long, killing it now.")
-                    #subproc.terminate()
-                    try:
-                        os.killpg(os.getpgid(subproc.pid),15)
-                    except:
-                        logger.warning("Killing the function call failed. It probably finished already.")
-                    finally:
-                        subproc.join()
-                        return(None)
-            return_value = parent_conn.recv()
-            subproc.join()
-            logger.debug("Your function has returned now with exit code %i."%subproc.exitcode)
+                    # if it is still alive, send sigterm for clean up
+                    # and return None to signal that it did not finish
+                    if not ready:
+                        logger.debug("Your function took to long, killing it now.")
+                        try:
+                            os.killpg(os.getpgid(subproc.pid),15)
+                            time.sleep(grace_period_in_s)
+                            logger.debug("Killing succesful!")
+                        except:
+                            logger.debug("Killing the function call failed. It probably finished already.")
+                        finally:
+                            return(None)
+                    # at this point there is something to be received
+                    return_value = parent_conn.recv()
+                    logger.debug("Your function has returned now with exit code %i."%subproc.exitcode)
+                
+                    # if something went wrong, 
+                    if subproc.exitcode != 0:
+                        logger.debug("Exit code was not 0 -> return value set to None!."%subproc.exitcode)
+                        return_value = None
+                else:
+                    signal.pause()
 
-            # if something went wrong, 
-            if subproc.exitcode != 0:
-                return(None)
+            except: # reraise everything else
+                raise
 
-            # return the function value from the pipe
-            return (return_value);
-
+            finally:
+                return (return_value); 
         return wrapped_function
-
     return actual_decorator
 
--- unit_tests.py
+++ unit_tests.py
@@ -13,14 +13,13 @@ all_tests=1
 
 
 # TODO: get rid of numpy just to create some data of fixed size!
-# 		add tests for cpu usage
-#		add test for num_processes
-#		add test with crash_unexpectedly to see what happens if the function call dies!
+#       add tests for cpu usage
+#       add test for num_processes
+#       add test with crash_unexpectedly to see what happens if the function call dies!
 
 def simulate_work(size_in_mb, wall_time_in_s, num_processes):
     # allocate memory (size_in_mb) with an array
     # note the actual size in memory of this process is a little bit larger
-    print("creating large array")
     A = [42.]*((1024*size_in_mb)//8);
 
     # try to spawn new processes
@@ -29,7 +28,7 @@ def simulate_work(size_in_mb, wall_time_in_s, num_processes):
         multiprocessing.Pool(num_processes)
 
     # sleep for specified duration
-    time.sleep(wall_time_in_s)
+    time.sleep(wall_time_in_s+1)
     return(size_in_mb, wall_time_in_s, num_processes);
 
 
@@ -41,97 +40,90 @@ def crash_unexpectedly(signum):
     time.sleep(1)
 
 # needs some improvements    
-def cpu_usage(wall_time_in_s,cpu_time_in_s ):
-    cpu_percentage = (100/wall_time_in_s)*cpu_time_in_s
-    return(cpu_percentage)
+def cpu_usage():
+    import random
+    while True:
+        x = random.random()
+        x = x*x
+
 
 class test_limit_resources_module(unittest.TestCase):
 
     @unittest.skipIf(not all_tests, "skipping successful tests")
     def test_success(self):
         
-        local_mem_in_mb = 512
-        local_time_in_s = None
-        local_grace_period = 1
+        print("Testing unbounded function call which have to run through!")
+        local_mem_in_mb = None
+        local_wall_time_in_s = None
+        local_cpu_time_in_s = None
+        local_grace_period = None
         
-        wrapped_function = pynisher.enforce_limits(mem_in_mb = local_mem_in_mb, wall_time_in_s=local_time_in_s, grace_period_in_s = local_grace_period)(simulate_work)
+        wrapped_function = pynisher.enforce_limits(mem_in_mb = local_mem_in_mb, wall_time_in_s=local_wall_time_in_s, cpu_time_in_s = local_cpu_time_in_s, grace_period_in_s = local_grace_period)(simulate_work)
         
         for mem in [1,2,4,8,16]:
-            print("mem: {}\n".format(mem))
             self.assertEqual((mem,0,0),wrapped_function(mem,0,0))
 
     @unittest.skipIf(not all_tests, "skipping out_of_memory test")
     def test_out_of_memory(self):
-
+        print("Testing memory constraint.")
         local_mem_in_mb = 64
-        local_wall_time_in_s = 2
+        local_wall_time_in_s = None
         local_cpu_time_in_s = None
-        local_grace_period = 0
+        local_grace_period = None
         
         wrapped_function = pynisher.enforce_limits(mem_in_mb = local_mem_in_mb, wall_time_in_s=local_wall_time_in_s, cpu_time_in_s = local_cpu_time_in_s, grace_period_in_s = local_grace_period)(simulate_work)
     
         for mem in [256,512,1024]:
-            print("\nmem: {}\n".format(mem))
             self.assertIsNone(wrapped_function(mem,0,0))
     
     @unittest.skipIf(not all_tests, "skipping time_out test")
     def test_time_out(self):
-
-        local_mem_in_mb = 0
-        local_time_in_s = 1
-        local_grace_period = 2
+        print("Testing wall clock time constraint.")
+        local_mem_in_mb = None
+        local_wall_time_in_s = 1
+        local_cpu_time_in_s = None
+        local_grace_period = None
         
-        wrapped_function = pynisher.enforce_limits(mem_in_mb = local_mem_in_mb, wall_time_in_s=local_time_in_s, grace_period_in_s = local_grace_period)(simulate_work)
+        wrapped_function = pynisher.enforce_limits(mem_in_mb = local_mem_in_mb, wall_time_in_s=local_wall_time_in_s, cpu_time_in_s = local_cpu_time_in_s, grace_period_in_s = local_grace_period)(simulate_work)
     
-        for mem in [64,128,256,512]:
-            print("\nmem: {}\n".format(mem))
-            self.assertIsNone(wrapped_function(mem,2,0))
+        for mem in range(1,10):
+            self.assertIsNone(wrapped_function(mem,10,0))
 
-    @unittest.skipIf(not all_tests, "skipping term signal test")
-    def test_term_signal(self):
-        
-        local_mem_in_mb = 0
-        local_wall_time_in_s = 0
-        local_cpu_time_in_s = 0
-        local_grace_period = 1
-        
-        wrapped_function = pynisher.enforce_limits(mem_in_mb = local_mem_in_mb, wall_time_in_s=local_wall_time_in_s, cpu_time_in_s = local_cpu_time_in_s, grace_period_in_s = local_grace_period)(crash_unexpectedly)
-        self.assertIsNone(wrapped_function(signal.SIGTERM))
-    
     @unittest.skipIf(not all_tests, "skipping too many processes test")
     def test_num_processes(self):
-        local_mem_in_mb = 350
+        print("Testing number of processes constraint.")
+        local_mem_in_mb = None
         local_num_processes = 1
-        local_wall_time_in_s = 20
-        local_grace_period = 20
+        local_wall_time_in_s = None
+        local_grace_period = None
         
         wrapped_function = pynisher.enforce_limits(mem_in_mb = local_mem_in_mb, wall_time_in_s=local_wall_time_in_s,num_processes = local_num_processes, grace_period_in_s = local_grace_period)(simulate_work)
         
         for processes in [2,15,50,100,250]:
-            print("\nprocesses: {}\n".format(processes))
             self.assertIsNone(wrapped_function(0,0, processes))
-            
 
     @unittest.skipIf(not all_tests, "skipping unexpected signal test")
     def test_crash_unexpectedly(self):
+        print("Testing an unexpected signal simulating a crash.")
         wrapped_function = pynisher.enforce_limits()(crash_unexpectedly)
         self.assertIsNone(wrapped_function(signal.SIGQUIT))
         
-
-
-# what is that?
-"""
-class test_cpu_usage(unittest.TestCase):
+    @unittest.skipIf(not all_tests, "skipping unexpected signal test")
     def test_high_cpu_percentage(self):
-        mem_in_mb = 35
-        wall_time_in_s = 10
-        cpu_time_in_s = 10
-        grace_period = 1
-        wrapped_function = pynisher.enforce_limits(mem_in_mb = mem_in_mb, wall_time_in_s=wall_time_in_s, cpu_time_in_s = cpu_time_in_s, grace_period_in_s = grace_period)(cpu_usage)
-        self.assertEqual(75,wrapped_function(20,15))
+        print("Testing cpu time constraint.")
+        mem_in_mb = None
+        wall_time_in_s = None
+        cpu_time_in_s = 1
+        grace_period = None
+        wrapped_function = pynisher.enforce_limits(cpu_time_in_s = cpu_time_in_s, grace_period_in_s = grace_period)(cpu_usage)
+        self.assertEqual(None,wrapped_function())
 
-"""     
-logger = multiprocessing.log_to_stderr()
-logger.setLevel(logging.DEBUG)
+
+#logger = multiprocessing.log_to_stderr()
+#logger.setLevel(logging.INFO)
 
 unittest.main()
+
+#wrapped_function = pynisher.enforce_limits(wall_time_in_s = 10, num_processes = 1)(simulate_work)       
+#print(wrapped_function(0,0, 5))
+
