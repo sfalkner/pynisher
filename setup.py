f05d7dacc42a94ac843ae49a71894924e08de559
Stefan Falkner <sfalkner@informatik.uni-freiburg.de>
2016-02-15 21:16:55 +0100

    added psutil as a requirement

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name = 'pynisher',
     version = "0.3.1",
     packages = ['pynisher'],
-    install_requires = ['docutils>=0.3', 'setuptools'],
+    install_requires = ['docutils>=0.3', 'setuptools', 'psutil'],
     author = "Stefan Falkner",
     author_email = "sfalkner@informatik.uni-freiburg.de",
     description = "A small Python library to limit the resources used by a function by executing it inside a subprocess.",
