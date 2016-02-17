96f2f8148743c418acb8593f3b8f3c9c3e12c4fc
Stefan Falkner <sfalkner@cs.uni-freiburg.de>
2016-02-17 16:29:32 +0100

    transition to class based enforce_limits with additional functionality updated the readme file to serve as documentation

--- README.rst
+++ README.rst
@@ -1,10 +1,14 @@
-The pynisher is a little module intended to limit a functions resources. It starts a new process, sets the desired limits, and executes the function inside it. In the end, it returns the function return value. If, for any reason, the function call is not successful, None is returned.
+The pynisher is a little module intended to limit a functions resources.
+It starts a new process, sets the desired limits, and executes the
+function inside it. In the end, it returns the function return value.
+If, for any reason, the function call is not successful, None is returned.
 
-Currently, the total memory usage(*), the wall clock time, the cpu time and the number of processes can be limited.
+Currently, the total memory usage(*), wall and cpu time, and the number of subprocesses can be limited.
 
 
 (*) As the subprocess also includes the Python interpreter, the actual memory available to your function is less than the specified value.
 
+To show the basic usage, consider the following script
 
 .. code-block:: python
 
@@ -20,14 +24,59 @@ Currently, the total memory usage(*), the wall clock time, the cpu time and the 
         for t in range(5):
         	print(my_function(t))
 
-        # a more explicit usage
+The full list of argments to enforce_limits reads: 
 
-        def my_other_function(t):
-	        time.sleep(t)
-	        return(t)
+.. code-block:: python
 
-        # explicitly create a new function without wrapping the original everytime
-        my_wrapped_function = pynisher.enforce_limits(wall_time_in_s=3)(my_other_function)
+		pynisher.enforce_limits(mem_in_mb=None, cpu_time_in_s=None,\
+					wall_time_in_s=None, num_processes=None,\
+					grace_period_in_s = None, logger = None)
 
-        for t in range(5):
-	        print(my_wrapped_function(t))
+The first four are actual constraints on the memory, the CPU time, the wall time, and the
+number of subprocesses of the function. All values should be integers or None, which means
+no restriction.
+
+The grace period allows the function to properly end. More technically, the subprocess receives
+a SIGXCPU/SIGALARM signal if the CPU/wall clock limit is reached. After the grace period a
+SIGKILL is send terminating the process immediately. Without a grace period, pynisher might
+not be able to correctly determine the cause of the shutdown, as the subprocess might die without
+any notice (more on that below).
+
+The logger is used to display additional information about the status of the pynisher module
+(mostly debug level). This might come in handy while debugging.
+
+If you need to know what happend to the function call or why it was aborted,
+you can use the object returned from pynisher.enforce_limits. Consider this
+slight variation of the above example:
+
+.. code-block:: python
+
+		import pynisher
+		import time
+
+		def my_function (t):
+			time.sleep(t)
+			return(t)
+
+		for t in range(5):
+			obj = pynisher.enforce_limits(wall_time_in_s=2)(my_function)
+			result = obj(t)
+			print(result, obj.relut, obj.exit_status)
+		
+		# see all the available information
+		print(vars(f))
+
+The object ```obj``` can be used as the original function. After calling it, it contains
+the actual result, but also an indicator of what happend. The ```exit_status``` attribute
+is either zero (function returned properly) or one of the following exceptions:
+
+.. code-block:: python
+
+		pynisher.CpuTimeoutException	# CPU time limit was reached
+		pynisher.TimeoutException	# Wall clock time limit exceeded
+		pynisher.MemorylimitException	# function hit the memory constraint
+		pynisher.SubprocessException	# function tried to spawn too many subprocesses
+		pynisher.AnythingException	# Something else went wrong, e.g., your function received a signal and just died.
+
+Here, the above issue about the grace period becomes interesting. Without it, it is likely that
+a AnythingException is returned where a Cpu-/TimeoutException would be appropriate.
--- pynisher/limit_function_call.py
+++ pynisher/limit_function_call.py
@@ -9,138 +9,185 @@ import sys
 
 import psutil
 
-class abort_function (Exception): pass
+class CpuTimeoutException (Exception): pass
+class TimeoutException (Exception): pass
+class MemorylimitException (Exception): pass
+class SubprocessException (Exception): pass
+class AnythingException (Exception): pass
 
 # create the function the subprocess can execute
-def subprocess_func(func, pipe, mem_in_mb, cpu_time_limit_in_s, wall_time_limit_in_s, num_procs, *args, **kwargs):
-    # returning logger used by multiprocessing (a new one is created)
-    logger = multiprocessing.get_logger()
+def subprocess_func(func, pipe, logger, mem_in_mb, cpu_time_limit_in_s, wall_time_limit_in_s, num_procs, grace_period_in_s,*args, **kwargs):
 
-    # simple signal handler to catch the signals for time limits
-    def handler(signum, frame):
-        # logs message with level debug on this logger 
-        logger = multiprocessing.get_logger()
-        logger.debug("received signal number %i. Exiting uncracefully."%signum)
-        
-        if (signum == signal.SIGXCPU):
-            # when process reaches soft limit --> a SIGXCPU signal is sent (it normally terminats the process)
-            logger.warning("CPU time exceeded, aborting!")
-        elif (signum == signal.SIGALRM):
-            # SIGALRM is sent to process when the specified time limit to an alarm function elapses (when real or clock time elapses)
-            logger.warning("Wallclock time exceeded, aborting!")
-        raise abort_function
+	# simple signal handler to catch the signals for time limits
+	def handler(signum, frame):
+		# logs message with level debug on this logger 
+		logger.debug("signal handler: %i"%signum)
+		if (signum == signal.SIGXCPU):
+			# when process reaches soft limit --> a SIGXCPU signal is sent (it normally terminats the process)
+			raise(CpuTimeoutException)
+		elif (signum == signal.SIGALRM):
+			# SIGALRM is sent to process when the specified time limit to an alarm function elapses (when real or clock time elapses)
+			logger.debug("timeout")
+			raise(TimeoutException)
+		raise AnythingException
 
 
-    # catching all signals at this point turned out to interfer with the subprocess (e.g. using ROS)
-    signal.signal(signal.SIGALRM, handler)
-    signal.signal(signal.SIGXCPU, handler)
+	# catching all signals at this point turned out to interfer with the subprocess (e.g. using ROS)
+	signal.signal(signal.SIGALRM, handler)
+	signal.signal(signal.SIGXCPU, handler)
+	signal.signal(signal.SIGQUIT, handler)
 
-    # set the memory limit
-    if mem_in_mb is not None:
-        # byte --> megabyte
-        mem_in_b = mem_in_mb*1024*1024
-        # the maximum area (in bytes) of address space which may be taken by the process.
-        resource.setrlimit(resource.RLIMIT_AS, (mem_in_b, mem_in_b))
+	# code to catch EVERY catchable signal (even X11 related ones ... )
+	# only use for debugging/testing as this seems to be too intrusive.
+	"""
+	for i in [x for x in dir(signal) if x.startswith("SIG")]:
+		try:
+			signum = getattr(signal,i)
+			print("register {}, {}".format(signum, i))
+			signal.signal(signum, handler)
+		except:
+			print("Skipping %s"%i)
+	"""
 
-    # for now: don't allow the function to spawn subprocesses itself.
-    #resource.setrlimit(resource.RLIMIT_NPROC, (1, 1))
-    # Turns out, this is quite restrictive, so we don't use this option by default
-    if num_procs is not None:
-        resource.setrlimit(resource.RLIMIT_NPROC, (num_procs, num_procs))
+	# set the memory limit
+	if mem_in_mb is not None:
+		# byte --> megabyte
+		mem_in_b = mem_in_mb*1024*1024
+		# the maximum area (in bytes) of address space which may be taken by the process.
+		resource.setrlimit(resource.RLIMIT_AS, (mem_in_b, mem_in_b))
 
-
-    # schedule an alarm in specified number of seconds
-    if wall_time_limit_in_s is not None:
-        signal.alarm(wall_time_limit_in_s)
-    
-    if cpu_time_limit_in_s is not None:
-        resource.setrlimit(resource.RLIMIT_CPU, (cpu_time_limit_in_s,cpu_time_limit_in_s))
-
-    return_value = None
-    # the actual function call
-    try:
-        logger.debug('call to your function')
-        return_value = func(*args, **kwargs)
-        logger.debug('function returned %s'%str(return_value))
-
-    except MemoryError:
-        logger.warning("Function call with the arguments {}, {} has exceeded the memory limit!".format(args,kwargs))
-
-    except OSError as e:
-        if (e.errno == 11):
-            logger.warning("Your function tries to spawn too many subprocesses/threads.")
-        else:
-            logger.debug('Something is going on here!')
-            raise;
-
-    except abort_function:
-        return_value = None
-        logger.warning('Your function call was aborted.')
-
-    except:
-        logger.debug('The call to your function did not return properly!\n%s\n%s', args, kwargs)
-        raise;
-    finally:
-        try:
-            pipe.send(return_value)
-            pipe.close()
-            # recursively kill all children
-            p = psutil.Process()
-            for child in p.children(recursive=True):
-                child.kill()
-        except:
-            pass
+	# for now: don't allow the function to spawn subprocesses itself.
+	#resource.setrlimit(resource.RLIMIT_NPROC, (1, 1))
+	# Turns out, this is quite restrictive, so we don't use this option by default
+	if num_procs is not None:
+		resource.setrlimit(resource.RLIMIT_NPROC, (num_procs, num_procs))
 
 
-def enforce_limits (mem_in_mb=None, cpu_time_in_s=None, wall_time_in_s=None, num_processes=None, grace_period_in_s = None):
+	# schedule an alarm in specified number of seconds
+	if wall_time_limit_in_s is not None:
+		signal.alarm(wall_time_limit_in_s)
+	
+	if cpu_time_limit_in_s is not None:
+		# From the Linux man page:
+		# When the process reaches the soft limit, it is sent a SIGXCPU signal.
+		# The default action for this signal is to terminate the process.
+		# However, the signal can be caught, and the handler can return control 
+		# to the main program. If the process continues to consume CPU time,
+		# it will be sent SIGXCPU once per second until the hard limit is reached,
+		# at which time it is sent SIGKILL.
+		resource.setrlimit(resource.RLIMIT_CPU, (cpu_time_limit_in_s,cpu_time_limit_in_s+grace_period_in_s))
 
-    logger = multiprocessing.get_logger()
-    
-    if mem_in_mb is not None:
-        logger.debug("restricting your function to {} mb memory.".format(mem_in_mb))
-    if cpu_time_in_s is not None:
-        logger.debug("restricting your function to {} seconds cpu time.".format(cpu_time_in_s))
-    if wall_time_in_s is not None:
-        logger.debug("restricting your function to {} seconds wall time.".format(wall_time_in_s))
-    if num_processes is not None:
-        logger.debug("restricting your function to {} threads/processes.".format(num_processes))
-    if grace_period_in_s is None:
-        grace_period_in_s = 0
-    
-    def actual_decorator(func):
+	# the actual function call
+	try:
+		logger.debug("call function")
+		return_value = ((func(*args, **kwargs), 0))
+		logger.debug("function returned properly: {}".format(return_value))
+	except MemoryError:
+		return_value = (None, MemorylimitException)
 
-        def wrapped_function(*args, **kwargs):
-            global return_value
-            logger = multiprocessing.get_logger()
-            
-            # create a pipe to retrieve the return value
-            parent_conn, child_conn = multiprocessing.Pipe()
+	except OSError as e:
+		if (e.errno == 11):
+			return_value = (None, SubprocessException)
+		else:
+			return_value = (None, AnyithingException)
 
-            # create and start the process
-            subproc = multiprocessing.Process(target=subprocess_func, name="pynisher function call", args = (func, child_conn,mem_in_mb, cpu_time_in_s, wall_time_in_s, num_processes) + args ,kwargs = kwargs)
-            logger.debug("Your function is called now.")
+	except CpuTimeoutException:
+		return_value = (None, CpuTimeoutException)
 
-            return_value = None
+	except TimeoutException:
+		return_value = (None, TimeoutException)
 
-            # start the process
-            subproc.start()
-            child_conn.close()
+	except AnythingException as e:
+		return_value = (None, AnythingException)
+	except:
+		raise
+		logger.debug("Some wired exception occured!")
+		
+	finally:
+		try:
+			logger.debug("return value: {}".format(return_value))
+			
+			pipe.send(return_value)
+			pipe.close()
 
-            try:
-                # read the return value
-                if parent_conn.poll(wall_time_in_s):
-                    return_value = parent_conn.recv()
-                else:
-                    subproc.terminate()
-                
-            except EOFError:    # Don't see that in the unit tests :(
-                logger.debug("Your function call closed the pipe prematurely -> None will be returned")
-                return_value = None
-            except:
-                raise
-            finally:
-                # don't leave zombies behind
-                subproc.join()
-                return (return_value); 
-        return wrapped_function
-    return actual_decorator
+		except:
+			# this part should only fail if the parent process is alread dead, so there is not much to do anymore :)
+			pass
+		finally:
+			# recursively kill all children
+			p = psutil.Process()
+			for child in p.children(recursive=True):
+				child.kill()
+			
+
+class enforce_limits (object):
+	def __init__(self, mem_in_mb=None, cpu_time_in_s=None, wall_time_in_s=None, num_processes=None, grace_period_in_s = None, logger = None):
+		self.mem_in_mb = mem_in_mb
+		self.cpu_time_in_s = cpu_time_in_s
+		self.num_processes = num_processes
+		self.wall_time_in_s = wall_time_in_s
+		self.grace_period_in_s = 0 if grace_period_in_s is None else grace_period_in_s
+		self.logger = logger if logger is not None else multiprocessing.get_logger()
+		
+		if self.mem_in_mb is not None:
+			self.logger.debug("Restricting your function to {} mb memory.".format(self.mem_in_mb))
+		if self.cpu_time_in_s is not None:
+			self.logger.debug("Restricting your function to {} seconds cpu time.".format(self.cpu_time_in_s))
+		if self.wall_time_in_s is not None:
+			self.logger.debug("Restricting your function to {} seconds wall time.".format(self.wall_time_in_s))
+		if self.num_processes is not None:
+			self.logger.debug("Restricting your function to {} threads/processes.".format(self.num_processes))
+		if self.grace_period_in_s is not None:
+			self.logger.debug("Allowing a grace period of {} seconds.".format(self.grace_period_in_s))
+
+		
+	def __call__ (self, func):
+		
+		class function_wrapper(object):
+			def __init__(self2, func):
+				self2.func = func
+				self2.result = None
+				self2.exit_status = None
+			
+			def __call__(self2, *args, **kwargs):
+			
+				# create a pipe to retrieve the return value
+				parent_conn, child_conn = multiprocessing.Pipe()
+
+				# create and start the process
+				subproc = multiprocessing.Process(target=subprocess_func, name="pynisher function call", args = (self2.func, child_conn, self.logger, self.mem_in_mb, self.cpu_time_in_s, self.wall_time_in_s, self.num_processes, self.grace_period_in_s) + args ,kwargs = kwargs)
+				self.logger.debug("Function called with argumen: {}, {}".format(args, kwargs))
+
+
+				# start the process
+				subproc.start()
+				child_conn.close()
+
+				try:
+					# read the return value
+					if (self.wall_time_in_s is not None):
+						if parent_conn.poll(self.wall_time_in_s+self.grace_period_in_s):
+							self2.result, self2.exit_status = parent_conn.recv()
+						else:
+							subproc.terminate()
+							self2.exit_status = TimeoutException
+							
+					else:
+						self2.result, self2.exit_status = parent_conn.recv()
+
+				except EOFError:    # Don't see that in the unit tests :(
+					self.logger.debug("Your function call closed the pipe prematurely -> Subprocess probably got an uncatchable signal.")
+					
+					self2.exit_status = AnythingException
+
+				except:
+					self.logger.debug("Something else went wrong, sorry.")
+				finally:
+					self2.resources_function = resource.getrusage(resource.RUSAGE_CHILDREN)
+					self2.exit_status = 5 if self2.exit_status is None else self2.exit_status
+					# don't leave zombies behind
+					subproc.join()
+				return (self2.result); 
+		
+		
+		return (function_wrapper(func))
